2bd360d665baee4cd6d617e0303b59505cb87ec0
nvodoor <nvodoor@gmail.com>
2017-02-13 18:55:37 -0800

    completed part of the bare minimum

--- src/sillyDancer.js
+++ src/sillyDancer.js
@@ -1,10 +1,10 @@
 //THIS IS THE SUB CLASS
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+var makeSillyDancer = function(top, left, timeBetweenSteps) {
   // var blinkyDancer = makeDancer(top, left, timeBetweenSteps); 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   this.timeBetweenSteps = timeBetweenSteps;
-  makeDancer.call(this, top, left, timeBetweenSteps);
+  makeDancer.call(this);
   // blinkyDancer.step = function() {
   //   // call the old version of step at the beginning of any call to this new version of step
   //var oldStep = this.step();
@@ -17,9 +17,9 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   // return blinkyDancer;
 };
 
-makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
-makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
-makeBlinkyDancer.prototype.step = function() {
+makeSillyDancer.prototype = Object.create(makeDancer.prototype);
+makeSillyDancer.prototype.constructor = makeSillyDancer;
+makeSillyDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this, this.timeBetweenSteps);
   this.$node.toggle();
 };
