fc18499ad7e92c2436d2bbf630a26feb9ec0e465
nvodoor <nvodoor@gmail.com>
2017-02-14 19:46:55 -0800

    Cleaned up code.

--- src/makeBearDancer.js
+++ src/makeBearDancer.js
@@ -2,14 +2,11 @@
 var makeBearDancer = function(top, left, timeBetweenSteps) {
   makeDancer.call(this, top, left, timeBetweenSteps);
   this.$node = $('<img class="beardancer" src="./src/giphy.gif"></img>');
-  this.setPosition(top, left)
+  this.setPosition(top, left);
 };
 
 makeBearDancer.prototype = Object.create(makeDancer.prototype);
 makeBearDancer.prototype.constructor = makeBearDancer;
 makeBearDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this, this.timeBetweenSteps);
-};
-makeBearDancer.prototype.fade = function() {
-
 };
